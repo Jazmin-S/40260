b4ebbafc580337df088c7d46fe27e4add88d4b77
Jazmin <zs23014159@estudiantes.uv.mx>
2025-05-19 13:18:14 -0600

    07-formulario

--- html00/Formulario.js
+++ html00/Formulario.js
@@ -1,11 +1,33 @@
-console.log("Inicio")
-const boton = document.getElementById("enviar")
+console.log("inicio")
 
-boton.addEventListener("click", () => {
-    console.log("los parametros")
-    let user=document.getElementById("user")
-    let password=document.getElementById("password")
-    console.log(password.value)
-    console.log(user.value)
-}
-)
+/* const boton = document.getElementById("enviar")
+boton.addEventListener("click", 
+    () => {
+        console.log("los parametros")
+        let user=document.getElementById("user")
+        let password=document.getElementById("password")
+        console.log(password.value)
+        console.log(user.value) */
+        
+
+
+
+
+    const mostrar =  () => {
+        console.log("los parametros")
+        let user=document.getElementById("user")
+        let password=document.getElementById("password")
+        console.log(password.value)
+        console.log(user.value)
+
+        let formulario = document.getElementsByTagName("form")
+        console.log(formulario)
+        user.setAttribute("name","q")
+        password.setAttribute("name","pasword")
+        formulario[0].setAttribute("action","https://google.com.mx/search")
+        formulario[0].submit()
+
+    }
+
+    const buton = document.getElementById("enviar")
+    buton.addEventListener("click", mostrar)
